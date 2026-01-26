8c76d17c6f67922b88174694e1e9bbcf5d764846
parniayzdin <pari.yazdinia@gmail.com>
2026-01-26 00:04:21 -0500

    add the first part of the simulator

---
+++ ResourceType/src/GameConfig.java
@@ -0,0 +1,31 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+public class GameConfig {
+    Path filePath = Paths.get(filename);
+    int turns;
+    public GameConfig(String filename){
+        try{
+            String content = Files.readString(filePath);
+            int i = content.indexOf("turns: ");
+
+            if (i == -1){
+                System.out.println("Missing the word 'turns' in the file");
+                return;
+            }
+
+            String after = content.substring(i + 6).trim();
+            String number = after;
+            turns = Integer.parseInt(number);
+
+        }catch(IOException e){
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+    }
+
+    public int getTurns(){
+        return turns;
+    }
+}
