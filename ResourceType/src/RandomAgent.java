42ee318c7b8c3d92d2e2f4abd530d63c5ecbcf79
parniayzdin <pari.yazdinia@gmail.com>
2026-01-26 23:50:23 -0500

    add the class for the R.8 rule

---
+++ ResourceType/src/RandomAgent.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class RandomAgent implements Agent{
+
+    @override
+    public String takeTurn(Player player, Random random){
+        ArrayList<String> actions = new ArrayList<>();
+
+        int total = player.totalCards();
+        boolean mustSpend = total > 7;
+
+        boolean canMakeRoad = player.enoughCards(ResourceType.BRICK, 1) && player.enoughCards(ResourceType.WOOD, 1);
+        boolean canMakeSettlement = player.enoughCards(ResourceType.BRICK, 1) &&
+                player.enoughCards(ResourceType.WOOD, 1) &&
+                player.enoughCards(ResourceType.SHEEP, 1) &&
+                player.enoughCards(ResourceType.WHEAT, 1);
+
+        boolean canMakeCity = player.enoughCards(ResourceType.WHEAT, 2)&&
+                player.enoughCards(ResourceType.ORE, 3);
+
+        if (canMakeRoad){
+            actions.add("Build a ROAD by spending 1 BRICK and 1 WOOD");
+        }
+
+        if (canMakeCity){
+            actions.add("Build a CITY by spending 2 WHEAT and 3 ORE");
+        }
+
+        if (canMakeSettlement){
+            actions.add("Build a SETTLEMENT by spending 1 BRICK, 1 WOOD, 1 SHEEP and 1 WHEAT");
+        }
+
+        //if it must spend but it cant do anything, return a message
+        if (mustSpend && actions.size() == 0){
+            return "has more that seven cards but cannot build anything";
+        }
+    }
+
+}
